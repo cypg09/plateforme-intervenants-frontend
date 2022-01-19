2a822b76c0b58b1ddbbd27aade3baceada042e30
cypg09 <cypg09@protonmail.com>
2022-01-20 00:48:19 +0100

    Updated my- to be consistent (CSS)

--- pages/connexion/index.js
+++ pages/connexion/index.js
@@ -2,7 +2,7 @@ import H1 from "../../components/ui/H1";
 
 export default function Connexion() {
   return(
-    <div className="mt-8">
+    <div className="my-12">
       <H1 text="Connexion"/>
     </div>
   );
