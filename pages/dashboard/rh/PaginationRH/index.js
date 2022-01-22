e32498b46173c8f9823bce0f0461a44bd4283967
cypg09 <cypg09@protonmail.com>
2022-01-22 16:46:50 +0100

    Added useEffect to RhDashboardGrid

--- pages/dashboard/rh/PaginationRH/index.js
+++ pages/dashboard/rh/PaginationRH/index.js
@@ -1,11 +1,13 @@
 import PaginationButton from "./PaginationButton";
 
 export default function PaginationRH({ nombreDePagesTotal, currentPageNumber, setNewPageNumber }) {
+  console.log(nombreDePagesTotal);
   let pagination = [];
-  let minSlice = Math.max(1, currentPageNumber-3);
-  let maxSlice = Math.min(nombreDePagesTotal, currentPageNumber+3);
-  let initArray = Array.from(Array(nombreDePagesTotal).keys());
-  let defaultPagination = initArray.slice(minSlice, maxSlice);
+  let minSlice = Math.max(1, currentPageNumber-9);
+  let maxSlice = Math.min(nombreDePagesTotal, currentPageNumber+9);
+  let initArray = Array.from(Array(nombreDePagesTotal+1).keys());
+  let defaultPagination = initArray.slice(minSlice, maxSlice+1);
+  console.log(defaultPagination);
   for (let i of defaultPagination) {
     pagination.push(
       <PaginationButton
--- pages/dashboard/rh/RhEtudeCardsGrid.js
+++ pages/dashboard/rh/RhEtudeCardsGrid.js
@@ -1,19 +1,28 @@
+import { useEffect } from "react";
 import { useState } from "react/cjs/react.development";
 import PaginationRH from "./PaginationRH";
 import RhEtudeCard from "./RhEtudeCard";
 
 export default function RhEtudeCardsGrid() {
+  const [hasLoaded, setHasLoaded] = useState(false);
   const [currentPageNumber, setNewPageNumber] = useState(1);
-  const [nombreDePagesTotal, setNombredePagesTotal] = useState(1)
-  let nombreDePagesTotal = 1; //TODO API fetch
+  const [nombreDePagesTotal, setNombredePagesTotal] = useState(9);
 
-  const updateNombreDePagesTotal = () => {
-    nombreDePagesTotal = fetch("localhost:8000/etude/nombreEtude")
-      .then((res) => (res.json))
-      .then(setNombredePagesTotal(res.nombreEtudes));
+  
+  useEffect(() => {
+    renderPagination();
+    renderCards();
+  }, [])
+
+  
+
+  const renderPagination = () => {
+    fetch("http://127.0.0.1:8000/etude/nombreEtudes")
+      .then((res) => (res.json()))
+      .then((res) => {
+        setNombredePagesTotal(Math.max(res.nombreEtudes % 30, 1))
+      .catch((e) => console.warn("Error in updateNombreDePagesTotal: ", e))
   }
-
-  updateNombreDePagesTotal();
 
   // fetch cards from currentPageNumber to 30 if currentPageNumber === 1 else 1*currentPageNumber + 1 to 30*currentPageNumber
   const renderCards = (pageNumber) => {
@@ -23,6 +32,9 @@ export default function RhEtudeCardsGrid() {
     let cards = [];
     cards.push();
   }
+
+  if (!hasLoaded) return <Loading />;
+
   return(
     <>
       <div className="grid grid-cols-1 gap-3 my-3 mt-12 md:gap-9 md:grid-cols-2 lg:grid-cols-3">
@@ -32,7 +44,7 @@ export default function RhEtudeCardsGrid() {
           type={"Etude de marché"}
           remuneration={"400"}
           dateDeSignature={"02/03/2021"}
-          nombreDePostulants={12}
+          nombreDePostulants={2}
           nombreDePostulantsPremium={4}
         />
 
