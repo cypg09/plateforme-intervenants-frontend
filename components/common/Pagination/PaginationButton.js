072d1b42b1f3c3048ca75ac363e2b44537153da5
cypg09 <cypg09@protonmail.com>
2022-01-25 14:49:43 +0100

    Style Pagination

--- components/common/Pagination/PaginationButton.js
+++ components/common/Pagination/PaginationButton.js
@@ -1,7 +1,7 @@
 export default function PaginationButton({ targetPage, setNewPage, isCurrentPage =false }) {
-  let defaultClassName = "px-2 py-1 m-1 text-teal-400 rounded-md border border-teal-400 shadow hover:bg-teal-50" + " ";
+  let defaultClassName = "px-2 bg-white py-1 m-1 text-teal-400 rounded-md border border-teal-400 shadow hover:bg-teal-50" + " ";
   if (isCurrentPage) {
-    defaultClassName += "bg-teal-400 text-teal-50 hover:text-teal-400 hover:bg-teal-300";
+    defaultClassName += "bg-teal-400 text-teal-50 hover:bg-teal-300";
   }
   const handleClick = () => {
     setNewPage(targetPage);
