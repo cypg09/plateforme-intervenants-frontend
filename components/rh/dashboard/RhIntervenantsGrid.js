875ef7c3e91a8aa558e6fb728b3b5d6c48efc175
cypg09 <cypg09@protonmail.com>
2022-02-04 07:42:05 +0100

    Added state to RhIntervenantsGrid

--- components/rh/dashboard/RhIntervenantsGrid.js
+++ components/rh/dashboard/RhIntervenantsGrid.js
@@ -1,18 +1,27 @@
-export default function RhIntervenantsGrid() {
-  return(
-  <>
-    <div className="grid grid-cols-3 p-8 mt-16 space-y-4 bg-white rounded-2xl divide-y">
-      <p className="text-lg font-bold">Nom</p>
-      <p className="text-lg font-bold">Campus</p>
-      <p className="text-lg font-bold">Nombre de phases en ce moment</p>
+import React from "react";
 
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-      <p>Test</p>
-    </div>
-  </>
+export default function RhIntervenantsGrid() {
+  const [intervenants, setIntervenants] = React.useState([]);
+
+  //if (!intervenants.length) {
+  //  return(<p className="mt-16 text-lg">Pas encore d'intervenants inscrits !</p>)
+  //}
+
+  return(
+    <>
+      <div className="grid grid-cols-3 p-8 mt-16 bg-white rounded-2xl">
+        <p className="text-lg font-bold">Nom</p>
+        <p className="text-lg font-bold">Campus</p>
+        <p className="text-lg font-bold">Nombre de phases en ce moment</p>
+
+        <p className="my-2">Test</p>
+        <p className="my-2">Test</p>
+        <p className="my-2">3</p>
+
+        <p className="my-2">Test</p>
+        <p className="my-2">Test</p>
+        <p className="my-2">4</p>
+      </div>
+    </>
   );
 }
